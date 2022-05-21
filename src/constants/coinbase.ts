e015730a270be4e3180812a931e2bb28aa979bb7
nguyenvanhai21 <hai.nguyen@hdwebsoft.co>
2022-05-21 17:37:33 +0700

    Add cancel url for coinbase

--- src/constants/coinbase.ts
+++ src/constants/coinbase.ts
@@ -3,4 +3,5 @@ export const DEFAULT_CHARGE_DATA: any = {
   description: 'Payment for purchasing products',
   pricing_type: 'fixed_price',
   redirect_url: process.env.CLIENT_DOMAIN + '/payment/coinbase/callback/success', // here is the url that handle callback
+  cancel_url: process.env.CLIENT_DOMAIN + '/payment/coinbase/callback/cancel', // here is the url that handle callback
 };
