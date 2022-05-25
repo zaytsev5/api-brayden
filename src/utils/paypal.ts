9118c746403913460d8e2f4b2226e42a23150f06
nguyenvanhai21 <hai.nguyen@hdwebsoft.co>
2022-05-25 10:44:43 +0700

    Update env, paypal utils

--- src/utils/paypal.ts
+++ src/utils/paypal.ts
@@ -2,8 +2,8 @@ import paypal from 'paypal-rest-sdk';
 
 paypal.configure({
   mode: process.env.PAYPAL_MODE || 'sandbox',
-  client_id: 'AWgYYDvYvC35qGNvoTs8QDLUZdl8kmaOISELHK1lAA6GcEhjMc5eCR-c54eOVOLOuNyWQE7fpkoD5w_w' || '',
-  client_secret: 'EDvXzdrHt_E6fWCdiE5ifE27TceUXVCcea9_iO3jl0u4XlFRiFYcrz1Lo6uXaLKKVZ0zOKGh9HfjQdc1' || '',
+  client_id: process.env.PAYPAL_CLIENT_ID || '',
+  client_secret: process.env.PAYPAL_CLIENT_SECRET || '',
 });
 
 export default paypal;
